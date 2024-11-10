958c00121cd5a1032e7ac00cf17d926359ada2df
muliix <paco23456@outlook.com>
2024-11-09 19:03:48 -0600

    some fixes

--- app/blueprints/register_user/__init__.py
+++ app/blueprints/register_user/__init__.py
@@ -3,3 +3,7 @@ from app.blueprints.register_user.register_user import RegisterUserView
 
 register_user = Blueprint('register_user', __name__, template_folder='templates/register_user')
 register_user.add_url_rule('/registrar_usuario', view_func=RegisterUserView.as_view('register_user'))
+
+class View:
+    def get_blueprint(self):
+        return register_user
