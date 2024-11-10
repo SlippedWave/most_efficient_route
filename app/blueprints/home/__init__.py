c85d26e30394ec65a72394da08fa0abd259c192d
Maaaxgz <gonzalezbahena18100109@gmail.com>
2024-11-10 15:32:50 -0600

    Splash Screen y diseno en index.html

--- app/blueprints/home/__init__.py
+++ app/blueprints/home/__init__.py
@@ -1,8 +1,25 @@
-from flask import Blueprint
+# from flask import Blueprint
+# from app.blueprints.home.home import HomeView
+
+# home = Blueprint('home', __name__, template_folder='templates/home')
+# home.add_url_rule('/', view_func=HomeView.as_view('home'))
+
+# class View:
+#     def get_blueprint(self):
+#         return home
+
+from flask import Blueprint, render_template
 from app.blueprints.home.home import HomeView
 
 home = Blueprint('home', __name__, template_folder='templates/home')
-home.add_url_rule('/', view_func=HomeView.as_view('home'))
+
+# Ruta para el splash screen
+@home.route('/')
+def splash():
+    return render_template('splash.html')
+
+# Ruta para la página principal
+home.add_url_rule('/home', view_func=HomeView.as_view('home'))
 
 class View:
     def get_blueprint(self):
---
+++ app/static/js/splash.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Esperar 3 segundos y luego redireccionar
+    setTimeout(function() {
+        window.location.href = "/home"; // Cambia '/home' a la ruta de tu página principal
+    }, 5000);
+});
