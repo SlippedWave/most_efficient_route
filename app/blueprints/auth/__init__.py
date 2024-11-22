67eed8d781d1daf07db2d0f0a4e053206a10d210
muliix <paco23456@outlook.com>
2024-11-22 14:31:39 -0600

    bug solutions

--- app/blueprints/auth/__init__.py
+++ app/blueprints/auth/__init__.py
@@ -9,9 +9,13 @@ auth = Blueprint("auth", __name__, template_folder="templates/auth")
 auth.add_url_rule("/iniciar_sesion", view_func=LoginView.as_view("login"))
 auth.add_url_rule("/cerrar_sesion", view_func=LogoutView.as_view("logout"))
 auth.add_url_rule(
-    "/iniciar_sesion/recuperar_usuario", view_func=RecoverUserView.as_view("recover_user")
+    "/iniciar_sesion/recuperar_usuario",
+    view_func=RecoverUserView.as_view("recover_user"),
 )
-auth.add_url_rule('/iniciar_sesion/cambiar_contrasena/<token>', view_func=ResetPasswordView.as_view('reset_password'))
+auth.add_url_rule(
+    "/iniciar_sesion/cambiar_contrasena/<token>",
+    view_func=ResetPasswordView.as_view("reset_password"),
+)
 
 from app import login_manager
 
